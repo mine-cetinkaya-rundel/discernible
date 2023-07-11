a8700badf97b163bd757a5f7d682551ad20eb3d7
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2023-07-10 21:05:43 -0500

    Add info

--- R/discernible.R
+++ R/discernible.R
@@ -4,12 +4,16 @@
 #' in a given character string.
 #'
 #' @param x character string.
-#' @importFrom stringr str_replace_all
+#' @importFrom stringr str_replace_all str_detect
+#' @importFrom cli cli_alert_info
 #' @export
 #' @examples
 #' conclusion <- "The clinical trial showed statistically significant evidence of being effective."
 #' discernible(conclusion)
 
 discernible <- function(x){
+  if(str_detect(x, "tatistically significant")){
+    cli_alert_info("Replacing statistically significant with statistically discernible.")
+  }
   str_replace_all(x, "tatistically significant", "tatistically discernible")
 }
